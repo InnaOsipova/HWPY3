855907cca2e89e0261684f9b311b33e085302f29
Inna Osipova <rutheva_inna@mail.ru>
2022-09-09 14:00:37 +0300

    Разница между max и min дробной части

--- DZ3.py
+++ DZ3.py
@@ -4,9 +4,6 @@
 #- [1.1, 1.2, 3.1, 5, 10.01] => 0.19
 
 some_list = [float(input('введите элемент списка: ')) for _ in range(int(input('Введите кол-во элементов: ')))]
-#mx = max([round(i%1, 3) for i in some_list])
-#mn =  min([round(i%1, 3) for i in some_list if round(i%1, 3) != 0])
-#print(mx, mn)
 print(f' Разница между max и min дробной части = \
     {round(max([round(i%1, 3) for i in some_list]) - min([round(i%1, 3) for i in some_list if round(i%1, 3) != 0]), 3)}')
 
